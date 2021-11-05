a40fcf639ed14401f3111701b16b2d185e1d6a63
huanyinyueye <lightfugitive@gmail.com>
2021-11-05 08:10:39 +0800

    push

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# sidebar-navigation
+# Open the Sidebar Navigation Over a Part of the Content
+Git Page: https://huanyinyueye.github.io/sidebar-navigation/
---
+++ index.js
@@ -0,0 +1,40 @@
+const SB = document.getElementById("mySidebar");
+const OSB = document.getElementById("openbutton");
+const Menu1 = document.getElementById("menu1");
+const Menu2 = document.getElementById("menu2");
+const Menu3 = document.getElementById("menu3");
+const Youtube = document.getElementById("yt");
+const Title = document.getElementById("title");
+const Text = document.getElementById("text");
+
+function openSB() {
+    if (SB.style.marginLeft=="-250px" ) {
+        SB.style.marginLeft="0px";
+        OSB.style.marginLeft="250px";
+    }
+    else {
+        SB.style.marginLeft="-250px";
+        OSB.style.marginLeft="0px";
+    }
+}
+
+
+function Num1Menu(){
+    Title.innerHTML = "11/04";
+    Text.innerHTML = "樂天 VS 中信"
+    Youtube.src="https://www.youtube.com/embed/TEVQkPC1_ME";
+    Youtube.style.visibility="inherit";
+}
+
+function Num2Menu(){
+    Title.innerHTML = "11/04";
+    Text.innerHTML = "富邦 VS 味全"
+    Youtube.src="https://www.youtube.com/embed/rc1ny-FnILM";
+    Youtube.style.visibility="inherit";
+}
+function Num3Menu(){
+    Title.innerHTML = "11/03";
+    Text.innerHTML = "富邦 VS 中信"
+    Youtube.src="https://www.youtube.com/embed/1wKDApuqNm0";
+    Youtube.style.visibility="inherit";
+}
